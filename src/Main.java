d88755fc4445dfe2c22ae612b4daeb4d81a8f5ee
clewd <c1ewd@protonmail.com>
2018-12-24 09:36:18 +0700

    Was added logical into attack method

--- src/Main.java
+++ src/Main.java
@@ -9,43 +9,43 @@ public class Main
 
         Panzer panzers[] = {
                 new Panzer("Russian 1", 100,
-                        100, 15, 5.0, 10.0),
+                        100, 30, 6.0, 20.0),
                 new Panzer("Russian 2", 100,
-                        100, 15, 5.0, 10.0),
+                        100, 15, 6.5, 15.0),
                 new Panzer("Russian 3", 100,
-                        100, 15, 5.0, 10.0),
-                new Panzer("Russian 4", 100,
-                        100, 15, 5.0, 10.0),
-                new Panzer("Russian 5", 100,
-                        100, 15, 5.0, 10.0),
-                new Panzer("Russian 6", 100,
-                        100, 15, 5.0, 10.0),
-                new Panzer("Russian 7", 100,
-                        100, 15, 5.0, 10.0),
-                new Panzer("Russian 8", 100,
-                        100, 15, 5.0, 10.0),
-                new Panzer("Russian 9", 100,
-                        100, 15, 5.0, 10.0),
+                        100, 17, 5.0, 15.0),
+                new Panzer("Russian 4", 80,
+                        100, 10, 5.0, 15.0),
+                new Panzer("Russian 5", 80,
+                        100, 10, 5.0, 9.0),
+                new Panzer("Russian 6", 80,
+                        100, 7, 4.7, 9.0),
+                new Panzer("Russian 7", 80,
+                        100, 7, 4.7, 8.0),
+                new Panzer("Russian 8", 75,
+                        100, 7, 3.0, 7.0),
+                new Panzer("Russian 9", 75,
+                        100, 7, 2.0, 6.0),
         };
         Panzer enemyPanzers[] = {
-                new Panzer("German 1", 100,
-                        100, 15, 5.0, 10.0),
+                new Panzer("German 1", 130,
+                        100, 20, 6.5, 10.0),
                 new Panzer("German 2", 100,
+                        100, 13, 6.5, 10.0),
+                new Panzer("German 3", 85,
+                        100, 20, 5.0, 10.0),
+                new Panzer("German 4", 85,
                         100, 15, 5.0, 10.0),
-                new Panzer("German 3", 100,
-                        100, 15, 5.0, 10.0),
-                new Panzer("German 4", 100,
-                        100, 15, 5.0, 10.0),
-                new Panzer("German 5", 100,
-                        100, 15, 5.0, 10.0),
-                new Panzer("German 6", 100,
-                        100, 15, 5.0, 10.0),
-                new Panzer("German 7", 100,
-                        100, 15, 5.0, 10.0),
-                new Panzer("German 8", 100,
-                        100, 15, 5.0, 10.0),
-                new Panzer("German 9", 100,
-                        100, 15, 5.0, 10.0),
+                new Panzer("German 5", 75,
+                        100, 15, 5.0, 9.0),
+                new Panzer("German 6", 75,
+                        100, 15, 5.0, 9.0),
+                new Panzer("German 7", 80,
+                        100, 10, 4.3, 9.0),
+                new Panzer("German 8", 70,
+                        100, 20, 4.0, 7.0),
+                new Panzer("German 9", 70,
+                        100, 7, 1.0, 6.0),
         };
 
         BattleField battle = new BattleField(panzers, enemyPanzers);
@@ -75,27 +75,48 @@ abstract class AbstractPanzer
         this.turnaroundTime = turnaroundTime;
     }
 
-    abstract int attack();
+    abstract int attack(Panzer panzer);
     abstract void defense();
 }
 
 interface Playable
 {
-    int attack();
+    int attack(Panzer panzer);
     void defense();
 }
 
 class Panzer extends AbstractPanzer implements Playable
 {
-
-    Panzer(String name, int health, int armor, int damage,
-           double shotTime, double turnaroundTime)
+    Random rnd;
+    Panzer(String name, int health, int armor, int damage, double shotTime, double turnaroundTime)
     {
-        super(name, health, armor, damage,
-                shotTime, turnaroundTime);
+        super(name, health, armor, damage, shotTime, turnaroundTime);
+        rnd = new Random(System.currentTimeMillis());
     }
     @Override
-    public int attack() {
+    public int attack(Panzer panzer)
+    {
+        switch (rnd.nextInt(3)) {
+            case 0:
+                System.out.println("Head to head attack: damage: " +
+                        (damage)/(panzer.armor + shotTime) * 10);
+
+                panzer.health -= (damage)/(panzer.armor + shotTime) * 10;
+                break;
+            case 1:
+                System.out.println("Side attack: damage: " +
+                        (damage)/(panzer.armor * panzer.turnaroundTime + shotTime) * 10);
+                panzer.health -= (damage)/(panzer.armor * panzer.turnaroundTime + shotTime) * 10;
+                health += (damage * shotTime)/(panzer.armor * panzer.turnaroundTime) * 10;
+                break;
+            case 2:
+                System.out.println("Attack from behind: damage: " +
+                        (damage)/(panzer.armor * panzer.turnaroundTime + shotTime) * 10);
+                panzer.health -= (damage)/(panzer.armor * panzer.turnaroundTime + shotTime) * 10;
+                health += (damage * shotTime)/(panzer.armor * panzer.turnaroundTime) * 10;
+                break;
+        }
+
         return 0;
     }
 
@@ -132,16 +153,31 @@ class BattleField
 
     void battle()
     {
+        int random;
+        int randomEnemy;
         do {
 
-            System.out.println(team[swith].name + " is attacking the enemy");
+            while(team[swith^1].panzers[random = rnd.nextInt(9)].health <= 0);
+
+            System.out.print("Panzer " + team[swith].panzers[random].name + " with "
+                    + team[swith].panzers[random].health + " health from " + team[swith].name + " is attacking ");
+
+            while(team[swith^1].panzers[randomEnemy = rnd.nextInt(9)].health <= 0);
+
+            System.out.println("enemy panzer " + team[swith^1].panzers[randomEnemy].name + " with "
+                    + team[swith^1].panzers[randomEnemy].health + " health from " + team[swith^1].name);
+
+
+            team[swith].panzers[random].attack(team[swith^1].panzers[randomEnemy]);
+
+//            System.out.println("New step");
 
             swith ^= 1;
 
-            for(int i = 0; i < team[0].panzers.length; i++)
-            {
-                team[0].panzers[i].health -= 10;
-            }
+//            for(int i = 0; i < team[0].panzers.length; i++)
+//            {
+//                team[0].panzers[i].health -= 10;
+//            }
 
             for (int i = 0; i < 2; i++)
             {
@@ -152,6 +188,7 @@ class BattleField
                 }
                 if (panzersHelath <= 0)
                 {
+                    System.out.println(team[i^1].name + " WIN!");
                     endBattle = true;
                     break;
                 }
